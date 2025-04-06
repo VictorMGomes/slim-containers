3d9b12a3513bc206883cdce64b1777b372ce4475
Victor M. Gomes <victorvhmgomes@gmail.com>
2025-04-06 15:35:56 -0300

    Refactor: fix node.js and add postgresql

--- source/services/nodejs/entrypoint.d/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Execute the command from the environment variable
-cd "/var/www/$GENERAL_APP_NAME" || exit 1
-
-echo "Running command: ${NODE_START_COMMAND}"
-exec ${NODE_START_COMMAND}
